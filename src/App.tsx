06fad86b9a25d21c3ec467bef6cc5434e134f4e8
ravkami@yandex.ru <ravkami@yandex.ru>
2024-02-04 22:13:09 +0300

    -try to host in github

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import React from "react";
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { Home } from "./pages/home/components/home";
 import { MonthStatictics } from "./pages/MonthStatictics/components/MonthStatictics";
 import { Header } from "./shared/Header";
 import { Popup } from "./shared/Popup/Popup";
-import { TestingS } from "./API/TestingS";
 function App() {
   return (
     <div className="global-container">
