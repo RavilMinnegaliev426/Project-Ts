8839a8439c8a5ddd513ca55a851ad8b0ac3a5225
ravkami@yandex.ru <ravkami@yandex.ru>
2024-02-08 00:41:07 +0300

    deploy app in host

--- src/API/WeatherCardService.tsx
+++ src/API/WeatherCardService.tsx
@@ -7,6 +7,11 @@ export async function getCardCurrentWeather(
   tabsNumber: number
 ): Promise<AxiosResponse<CardWeather>> {
   return axios.get<CardWeather>(
-    `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&daily=weathercode,temperature_2m_max,temperature_2m_min,sunset,sunrise,precipitation_probability_max,windspeed_10m_max,winddirection_10m_dominant&current_weather=true&forecast_days=${tabsNumber}&timezone=Europe%2FLondon`
+    `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&daily=weathercode,temperature_2m_max,temperature_2m_min,sunset,sunrise,precipitation_probability_max,windspeed_10m_max,winddirection_10m_dominant&current_weather=true&forecast_days=${tabsNumber}&timezone=Europe%2FLondon`,
+    {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
   );
 }
--- src/API/WeatherService.tsx
+++ src/API/WeatherService.tsx
@@ -5,6 +5,11 @@ export async function getCurrentWeather(
   city: string
 ): Promise<AxiosResponse<Weather>> {
   return axios.get<Weather>(
-    `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=14eb8319ec32a270aaceb55aa393688b&lang=ru`
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=14eb8319ec32a270aaceb55aa393688b&lang=ru`,
+    {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
   );
 }
